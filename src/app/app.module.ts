0979b7a997d086a95214393d2eea3ef9f4c6ef71
pongthorn <oumail002@gmail.com>
2024-11-05 18:55:45 +0700

    Fixed code error
    Fixed code error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StaffComponent } from './staff/staff.component';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
 // import { UserComponent } from './user/user.component';
 import { UserModule } from './user/user.module';
-=======
-import { UserComponent } from './user/user.component';
->>>>>>> fb771c11116584bc49e9d2a7120afa9728790857
 // import { ProductComponent } from './product/product.component';
 import { ProductModule } from './product/product.module';
 import { PagenofoundComponent } from './pagenofound/pagenofound.component';
@@ -34,20 +30,13 @@ const appRoutes: Routes = [
     AppComponent,
     StaffComponent,
     HomeComponent,
-<<<<<<< HEAD
     // UserComponent,
-=======
-    UserComponent,
->>>>>>> fb771c11116584bc49e9d2a7120afa9728790857
     // ProductComponent,
     PagenofoundComponent
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
     UserModule,
-=======
->>>>>>> fb771c11116584bc49e9d2a7120afa9728790857
     ProductModule,
     AppRoutingModule,
     // RouterModule.forRoot(
